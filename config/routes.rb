decf2474793e68aa19142777c68e9f123aeb6994
PhongNguyenTrung <90634242+PhongNguyenTrung@users.noreply.github.com>
2023-02-22 11:06:40 +0700

    Add a Static Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/help'
   devise_for :shops
   devise_for :customers
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
