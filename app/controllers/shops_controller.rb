f57299b7aeffa739aa8eca710cc9e1d3468d0b7e
PhongNguyenTrung <90634242+PhongNguyenTrung@users.noreply.github.com>
2023-03-01 09:08:31 +0700

    Create sign up, Login, Logout,  Edit Information

---
+++ app/controllers/shops_controller.rb
@@ -0,0 +1,7 @@
+class ShopsController < ApplicationController
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  get 'shops/new'
+  get 'shops/edit'
+  devise_for :users
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  # devise_for :shops
-  devise_for :customers
+  get 'help', to: 'static_pages#help'
+  # as :user do
+  #   get 'signin' => 'devise/sessions#new'
+  #   post 'signin' => 'devise/sessions#create'
+  #   delete 'signout' => 'devise/sessions#destroy'
+  # end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
