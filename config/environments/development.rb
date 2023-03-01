38b84a82fc69611ca8ec8caed3878578cca40e1b
PhongNguyenTrung <90634242+PhongNguyenTrung@users.noreply.github.com>
2023-03-01 12:13:06 +0700

    Create sign up, Login, Logout,  Edit Information, Forgot password using mailcatcher

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -64,6 +64,9 @@ Rails.application.configure do
   config.sass.inline_source_maps = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { address: 'mailcatcher', port: 1025 }
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
